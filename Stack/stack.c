be3148fa4255c8ee3521f5db30abb2bec8144558
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2021-01-21 17:08:18 -0400

    Adding .c and .h Stack files
    Creating the .c and .h stack files with initial headers and inteded functions.

---
+++ Stack/stack.c
@@ -0,0 +1,12 @@
+/****************************************************************************************************
+ * Author:      Pedro Lucas de Moliner de Castro                                                    *
+ * Copyright:   Open source code under MIT License                                                  *
+ * Description: Generic dynamic array implementation of a stack ADT for C                           *
+-****************************************************************************************************/
+
+#include "stack.h"
+
+struct STACK
+{
+    
+};
---
+++ Stack/stack.h
@@ -0,0 +1,25 @@
+/****************************************************************************************************
+ * Author:      Pedro Lucas de Moliner de Castro                                                    *
+ * Copyright:   Open source code under MIT License                                                  *
+ * Description: Generic dynamic array implementation of a stack ADT for C                           *
+-****************************************************************************************************/
+
+#ifndef _STACK_H_
+#define _STACK_H_
+
+typedef struct STACK stack_t;
+
+//init
+//empty
+//free
+//push
+//pop
+//peek
+//copy
+//invert
+//isEmpty
+//isFull
+//size
+//print
+
+#endif //_STACK_H_
