c4cc790e595c18ee67c64329da978117573b9b13
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2021-04-19 19:55:01 -0400

    Adding stack .c and .h source files
    Implementing the stack as a linked list in stack.c and adding its function headers to stack.h

--- Stack/stack.h
+++ Stack/stack.h
@@ -1,25 +1,33 @@
 /****************************************************************************************************
- * Author:      Pedro Lucas de Moliner de Castro                                                    *
- * Copyright:   Open source code under MIT License                                                  *
- * Description: Generic dynamic array implementation of a stack ADT for C                           *
--****************************************************************************************************/
+Author: Pedro Lucas de Moliner de Castro
+Copyright: Open source code under MIT License
+Description: Generic dynamic implementation of a stack in C
+*****************************************************************************************************/
 
-#ifndef _STACK_H_
-#define _STACK_H_
+#ifndef STACK_H
+#define STACK_H
 
-typedef struct STACK stack_t;
+#include <stddef.h>
+#include <stdbool.h>
 
-//init
-//empty
-//free
-//push
-//pop
-//peek
-//copy
-//invert
-//isEmpty
-//isFull
-//size
-//print
+typedef struct _Stack stack_t;
 
-#endif //_STACK_H_
+stack_t *stack_init(size_t datasize, int (*compare)(const void *first, const void *second), void (*free_data)(void *data));
+void stack_clear(stack_t *stack);
+void stack_free(stack_t *stack);
+
+int stack_push(stack_t *stack, const void *data);
+int stack_pop(stack_t *stack, void *destination);
+int stack_peek(stack_t *stack, void *destination);
+
+stack_t *stack_clone(stack_t *stack);
+stack_t *stack_reverse(stack_t *stack);
+
+bool stack_contains(stack_t *stack, const void *key);
+
+size_t stack_size(stack_t *stack);
+
+bool stack_isempty(stack_t *stack);
+bool stack_isfull(stack_t *stack);
+
+#endif //STACK_H
