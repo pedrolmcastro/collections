584c855cf74cc49a3ebdb61dd57d4447e10cdc6e
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2021-08-18 14:05:37 -0400

    Adding create and delete queue functions

--- src/queue/queue.c
+++ src/queue/queue.c
@@ -1,5 +1,6 @@
 #include "queue.h"
 
+#include <errno.h>
 #include <stdlib.h>
 
 
@@ -17,3 +18,68 @@ struct _Queue {
     void (*free_data)(void *data);
     int (*compare)(const void *first, const void *second);
 };
+
+
+static void _free_node(_node_t *node, void (*free_data)(void *data));
+
+
+queue_t *queue_construct(size_t width, void (*free_data)(void *data), int (*compare)(const void *first, const void *second)) {
+    if (width == 0 || compare == NULL) {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    queue_t *queue = malloc(sizeof(queue_t));
+    if (queue == NULL) {
+        errno = ENOMEM;
+        return NULL;
+    }
+
+    queue->head = NULL;
+    queue->tail = NULL;
+    queue->size = 0;
+
+    queue->width = width;
+    queue->free_data = free_data;
+    queue->compare = compare;
+
+    return queue;
+}
+
+void queue_clear(queue_t *queue) {
+    if (queue == NULL) {
+        errno = EINVAL;
+        return;
+    }
+
+    _node_t *node = queue->head;
+    while (node != NULL) {
+        _node_t *remove = node;
+        node = node->next;
+
+        _free_node(remove, queue->free_data);
+    }
+
+    queue->head = NULL;
+    queue->tail = NULL;
+    queue->size = 0;
+}
+
+void queue_free(queue_t *queue) {
+    if (queue != NULL) {
+        queue_clear(queue);
+        free(queue);
+    }
+}
+
+
+static void _free_node(_node_t *node, void (*free_data)(void *data)) {
+    if (node != NULL) {
+        if (free_data != NULL) {
+            free_data(node->data);
+        }
+
+        free(node->data);
+        free(node);
+    }
+}
--- src/queue/queue.h
+++ src/queue/queue.h
@@ -1,6 +1,14 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <stddef.h>
+
+
 typedef struct _Queue queue_t;
+
+
+queue_t *queue_construct(size_t width, void (*free_data)(void *data), int (*compare)(const void *first, const void *second));
+void queue_clear(queue_t *queue);
+void queue_free(queue_t *queue);
 
 #endif // QUEUE_H
