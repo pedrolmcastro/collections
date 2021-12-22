64d79580e735a6cbd3be01bf37f0e8976aa423db
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2021-12-22 16:20:43 -0400

    Documenting Bit Array

--- source/bitarray/bitarray.c
+++ source/bitarray/bitarray.c
@@ -1,3 +1,12 @@
+/**
+ * @author Pedro Lucas de Moliner de Castro
+ * @copyright MIT License
+ * @file bitarray.c
+ * 
+ * @brief Bitarray implementation using bytes as buckets
+ */
+
+
 #include "bitarray.h"
 
 #include <errno.h>
@@ -20,12 +29,86 @@ static const size_t _BITS[] = {0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4};
 static const size_t _BYTE = 8;
 
 
-static byte_t _byte_sanitize(bitarray_t *bitarray, byte_t byte);
+// Auxiliary functions for bytes
+static byte_t _byte_sanitize(const bitarray_t *bitarray, byte_t byte);
 
-static bitarray_t *_bitarray_smaller(bitarray_t *first, bitarray_t *second);
-static bitarray_t *_bitarray_bigger(bitarray_t *first, bitarray_t *second);
+// Auxiliary functions for bitarrays
+static const bitarray_t *_bitarray_bigger(const bitarray_t *first, const bitarray_t *second);
+static const bitarray_t *_bitarray_smaller(const bitarray_t *first, const bitarray_t *second);
 static void _bitarray_sanitize(bitarray_t *bitarray);
 
+
+bool bitarray_all(const bitarray_t *bitarray) {
+    if (bitarray == NULL) {
+        errno = EINVAL;
+        return false;
+    }
+
+    for (size_t i = 0; i < bitarray->buckets - 1; i++) {
+        if (bitarray->values[i] != 0xFF) {
+            return false;
+        }
+    }
+
+    return bitarray->values[bitarray->buckets - 1] == _byte_sanitize(bitarray, 0xFF);
+}
+
+bitarray_t *bitarray_and(const bitarray_t *first, const bitarray_t *second) {
+    if (first == NULL || second == NULL) {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    const bitarray_t *smaller = _bitarray_smaller(first, second);
+    const bitarray_t *bigger = _bitarray_bigger(first, second);
+
+    bitarray_t *and = bitarray_copy(smaller);
+    if (and == NULL) {
+        // errno set in bitarray_copy()
+        return NULL;
+    }
+
+    for (size_t i = 0; i < smaller->buckets; i++) {
+        and->values[i] &= bigger->values[i];
+    }
+
+    return and;
+}
+
+bool bitarray_any(const bitarray_t *bitarray) {
+    if (bitarray == NULL) {
+        errno = EINVAL;
+        return false;
+    }
+
+    for (size_t i = 0; i < bitarray->buckets; i++) {
+        if (bitarray->values[i] != 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+size_t bitarray_buckets(const bitarray_t *bitarray) {
+    if (bitarray == NULL) {
+        errno = EINVAL;
+        return 0;
+    }
+
+    return bitarray->buckets;
+}
+
+bool bitarray_clear(bitarray_t *bitarray) {
+    if (bitarray == NULL) {
+        errno = EINVAL;
+        return false;
+    }
+
+    memset(bitarray->values, 0, bitarray->buckets * sizeof(byte_t));
+
+    return true;
+}
 
 bitarray_t *bitarray_construct(size_t size) {
     if (size == 0) {
@@ -52,7 +135,7 @@ bitarray_t *bitarray_construct(size_t size) {
     return bitarray;
 }
 
-bitarray_t *bitarray_copy(bitarray_t *bitarray) {
+bitarray_t *bitarray_copy(const bitarray_t *bitarray) {
     if (bitarray == NULL) {
         errno = EINVAL;
         return NULL;
@@ -69,77 +152,31 @@ bitarray_t *bitarray_copy(bitarray_t *bitarray) {
     return copy;
 }
 
-void bitarray_free(bitarray_t *bitarray) {
-    if (bitarray != NULL) {
-        free(bitarray->values);
-        free(bitarray);
-    }
-}
-
-
-bool bitarray_test(bitarray_t *bitarray, size_t index) {
-    if (bitarray == NULL || index >= bitarray->size) {
-        errno = EINVAL;
-        return false;
-    }
-
-    return (bitarray->values[index / _BYTE] >> (index % _BYTE)) & 1;
-}
-
-
-bool bitarray_any(bitarray_t *bitarray) {
+size_t bitarray_count(const bitarray_t *bitarray) {
     if (bitarray == NULL) {
         errno = EINVAL;
-        return false;
+        return 0;
     }
+
+    size_t bits = 0;
 
     for (size_t i = 0; i < bitarray->buckets; i++) {
-        if (bitarray->values[i] != 0) {
-            return true;
+        for (byte_t j = bitarray->values[i]; j != 0; j >>= 4) {
+            bits += _BITS[j & 0xF];
         }
     }
 
-    return false;
+    return bits;
 }
 
-bool bitarray_all(bitarray_t *bitarray) {
+bool bitarray_fill(bitarray_t *bitarray) {
     if (bitarray == NULL) {
         errno = EINVAL;
         return false;
     }
 
-    for (size_t i = 0; i < bitarray->buckets - 1; i++) {
-        if (bitarray->values[i] != 0xFF) {
-            return false;
-        }
-    }
-
-    return bitarray->values[bitarray->buckets - 1] == _byte_sanitize(bitarray, 0xFF);
-}
-
-bool bitarray_none(bitarray_t *bitarray) {
-    return !bitarray_any(bitarray);
-}
-
-
-bool bitarray_set(bitarray_t *bitarray, size_t index) {
-    if (bitarray == NULL || index >= bitarray->size) {
-        errno = EINVAL;
-        return false;
-    }
-
-    bitarray->values[index / _BYTE] |= (1 << (index % _BYTE));
-
-    return true;
-}
-
-bool bitarray_reset(bitarray_t *bitarray, size_t index) {
-    if (bitarray == NULL || index >= bitarray->size) {
-        errno = EINVAL;
-        return false;
-    }
-
-    bitarray->values[index / _BYTE] &= ~(1 << (index % _BYTE));
+    memset(bitarray->values, 0xFF, bitarray->buckets * sizeof(byte_t));
+    _bitarray_sanitize(bitarray);
 
     return true;
 }
@@ -155,74 +192,18 @@ bool bitarray_flip(bitarray_t *bitarray, size_t index) {
     return true;
 }
 
-
-bitarray_t *bitarray_and(bitarray_t *first, bitarray_t *second) {
-    if (first == NULL || second == NULL) {
-        errno = EINVAL;
-        return NULL;
+void bitarray_free(bitarray_t *bitarray) {
+    if (bitarray != NULL) {
+        free(bitarray->values);
+        free(bitarray);
     }
-
-    bitarray_t *smaller = _bitarray_smaller(first, second);
-    bitarray_t *bigger = _bitarray_bigger(first, second);
-
-    bitarray_t *and = bitarray_copy(smaller);
-    if (and == NULL) {
-        // errno set in bitarray_copy()
-        return NULL;
-    }
-
-    for (size_t i = 0; i < smaller->buckets; i++) {
-        and->values[i] &= bigger->values[i];
-    }
-
-    return and;
 }
 
-bitarray_t *bitarray_or(bitarray_t *first, bitarray_t *second) {
-    if (first == NULL || second == NULL) {
-        errno = EINVAL;
-        return NULL;
-    }
-
-    bitarray_t *smaller = _bitarray_smaller(first, second);
-    bitarray_t *bigger = _bitarray_bigger(first, second);
-
-    bitarray_t *or = bitarray_copy(bigger);
-    if (or == NULL) {
-        // errno set in bitarray_copy()
-        return NULL;
-    }
-
-    for (size_t i = 0; i < smaller->buckets; i++) {
-        or->values[i] |= smaller->values[i];
-    }
-
-    return or;
+bool bitarray_none(const bitarray_t *bitarray) {
+    return !bitarray_any(bitarray);
 }
 
-bitarray_t *bitarray_xor(bitarray_t *first, bitarray_t *second) {
-    if (first == NULL || second == NULL) {
-        errno = EINVAL;
-        return NULL;
-    }
-
-    bitarray_t *smaller = _bitarray_smaller(first, second);
-    bitarray_t *bigger = _bitarray_bigger(first, second);
-
-    bitarray_t *xor = bitarray_copy(bigger);
-    if (xor == NULL) {
-        // errno set in bitarray_copy()
-        return NULL;
-    }
-
-    for (size_t i = 0; i < smaller->buckets; i++) {
-        xor->values[i] ^= smaller->values[i];
-    }
-
-    return xor;
-}
-
-bitarray_t *bitarray_not(bitarray_t *bitarray) {
+bitarray_t *bitarray_not(const bitarray_t *bitarray) {
     if (bitarray == NULL) {
         errno = EINVAL;
         return NULL;
@@ -242,50 +223,51 @@ bitarray_t *bitarray_not(bitarray_t *bitarray) {
     return not;
 }
 
+bitarray_t *bitarray_or(const bitarray_t *first, const bitarray_t *second) {
+    if (first == NULL || second == NULL) {
+        errno = EINVAL;
+        return NULL;
+    }
 
-bool bitarray_fill(bitarray_t *bitarray) {
-    if (bitarray == NULL) {
+    const bitarray_t *smaller = _bitarray_smaller(first, second);
+    const bitarray_t *bigger = _bitarray_bigger(first, second);
+
+    bitarray_t *or = bitarray_copy(bigger);
+    if (or == NULL) {
+        // errno set in bitarray_copy()
+        return NULL;
+    }
+
+    for (size_t i = 0; i < smaller->buckets; i++) {
+        or->values[i] |= smaller->values[i];
+    }
+
+    return or;
+}
+
+bool bitarray_reset(bitarray_t *bitarray, size_t index) {
+    if (bitarray == NULL || index >= bitarray->size) {
         errno = EINVAL;
         return false;
     }
 
-    memset(bitarray->values, 0xFF, bitarray->buckets * sizeof(byte_t));
-    _bitarray_sanitize(bitarray);
+    bitarray->values[index / _BYTE] &= ~(1 << (index % _BYTE));
 
     return true;
 }
 
-bool bitarray_clear(bitarray_t *bitarray) {
-    if (bitarray == NULL) {
+bool bitarray_set(bitarray_t *bitarray, size_t index) {
+    if (bitarray == NULL || index >= bitarray->size) {
         errno = EINVAL;
         return false;
     }
 
-    memset(bitarray->values, 0, bitarray->buckets * sizeof(byte_t));
+    bitarray->values[index / _BYTE] |= (1 << (index % _BYTE));
 
     return true;
 }
 
-
-size_t bitarray_count(bitarray_t *bitarray) {
-    if (bitarray == NULL) {
-        errno = EINVAL;
-        return 0;
-    }
-
-    size_t bits = 0;
-
-    for (size_t i = 0; i < bitarray->buckets; i++) {
-        for (byte_t j = bitarray->values[i]; j != 0; j >>= 4) {
-            bits += _BITS[j & 0xF];
-        }
-    }
-
-    return bits;
-}
-
-
-size_t bitarray_size(bitarray_t *bitarray) {
+size_t bitarray_size(const bitarray_t *bitarray) {
     if (bitarray == NULL) {
         errno = EINVAL;
         return 0;
@@ -294,37 +276,59 @@ size_t bitarray_size(bitarray_t *bitarray) {
     return bitarray->size;
 }
 
-size_t bitarray_buckets(bitarray_t *bitarray) {
-    if (bitarray == NULL) {
+bool bitarray_test(const bitarray_t *bitarray, size_t index) {
+    if (bitarray == NULL || index >= bitarray->size) {
         errno = EINVAL;
-        return 0;
+        return false;
     }
 
-    return bitarray->buckets;
+    return (bitarray->values[index / _BYTE] >> (index % _BYTE)) & 1;
 }
 
-
-static byte_t _byte_sanitize(bitarray_t *bitarray, byte_t byte) {
-    return byte & ~(0xFF << (bitarray->size % _BYTE));
-}
-
-
-static bitarray_t *_bitarray_smaller(bitarray_t *first, bitarray_t *second) {
+bitarray_t *bitarray_xor(const bitarray_t *first, const bitarray_t *second) {
     if (first == NULL || second == NULL) {
         errno = EINVAL;
         return NULL;
     }
 
-    return first->size <= second->size ? first : second;
+    const bitarray_t *smaller = _bitarray_smaller(first, second);
+    const bitarray_t *bigger = _bitarray_bigger(first, second);
+
+    bitarray_t *xor = bitarray_copy(bigger);
+    if (xor == NULL) {
+        // errno set in bitarray_copy()
+        return NULL;
+    }
+
+    for (size_t i = 0; i < smaller->buckets; i++) {
+        xor->values[i] ^= smaller->values[i];
+    }
+
+    return xor;
 }
 
-static bitarray_t *_bitarray_bigger(bitarray_t *first, bitarray_t *second) {
+
+static byte_t _byte_sanitize(const bitarray_t *bitarray, byte_t byte) {
+    return byte & ~(0xFF << (bitarray->size % _BYTE));
+}
+
+
+static const bitarray_t *_bitarray_bigger(const bitarray_t *first, const bitarray_t *second) {
     if (first == NULL || second == NULL) {
         errno = EINVAL;
         return NULL;
     }
 
     return first->size > second->size ? first : second;
+}
+
+static const bitarray_t *_bitarray_smaller(const bitarray_t *first, const bitarray_t *second) {
+    if (first == NULL || second == NULL) {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    return first->size <= second->size ? first : second;
 }
 
 static void _bitarray_sanitize(bitarray_t *bitarray) {
--- source/bitarray/bitarray.h
+++ source/bitarray/bitarray.h
@@ -1,5 +1,15 @@
+/**
+ * @author Pedro Lucas de Moliner de Castro
+ * @copyright MIT License
+ * @file bitarray.h
+ * 
+ * @brief Bitarray implementation using bytes as buckets
+ */
+
+
 #ifndef BITARRAY_H
 #define BITARRAY_H
+
 
 #include <stddef.h>
 #include <stdbool.h>
@@ -8,32 +18,187 @@
 typedef struct _BitArray bitarray_t;
 
 
-bitarray_t *bitarray_construct(size_t size);
-bitarray_t *bitarray_copy(bitarray_t *bitarray);
+/**
+ * @brief Tests if all bits in bitarray are set to 1
+ * @throw EINVAL If bitarray is NULL
+ * 
+ * @param[in] bitarray Bitarray reference
+ * @return If all bits in bitarray are set to 1 or false in errors
+ */
+bool bitarray_all(const bitarray_t *bitarray);
 
-void bitarray_free(bitarray_t *bitarray);
+/**
+ * @brief Performs an and operation
+ * @throw EINVAL If first or second are NULL
+ * @throw ENOMEM If there is not enough memory
+ * 
+ * @param[in] first First operand reference
+ * @param[in] second Second operand reference
+ * @return Resultant bitarray reference or NULL in errors
+ */
+bitarray_t *bitarray_and(const bitarray_t *first, const bitarray_t *second);
 
-bool bitarray_test(bitarray_t *bitarray, size_t index);
+/**
+ * @brief Tests if any bit in bitarray is set to 1
+ * @throw EINVAL If bitarray is NULL
+ * 
+ * @param[in] bitarray Bitarray reference
+ * @return If any bit in bitarray is set to 1 or false in errors
+ */
+bool bitarray_any(const bitarray_t *bitarray);
 
-bool bitarray_any(bitarray_t *bitarray);
-bool bitarray_all(bitarray_t *bitarray);
-bool bitarray_none(bitarray_t *bitarray);
+/**
+ * @brief Gets the bitarray buckets
+ * @throw EINVAL If bitarray is NULL
+ * 
+ * @param[in] bitarray Bitarray reference
+ * @return The bitarray buckets or 0 in errors
+ */
+size_t bitarray_buckets(const bitarray_t *bitarray);
 
-bool bitarray_set(bitarray_t *bitarray, size_t index);
-bool bitarray_reset(bitarray_t *bitarray, size_t index);
-bool bitarray_flip(bitarray_t *bitarray, size_t index);
-
-bitarray_t *bitarray_and(bitarray_t *first, bitarray_t *second);
-bitarray_t *bitarray_or(bitarray_t *first, bitarray_t *second);
-bitarray_t *bitarray_xor(bitarray_t *first, bitarray_t *second);
-bitarray_t *bitarray_not(bitarray_t *bitarray);
-
-bool bitarray_fill(bitarray_t *bitarray);
+/**
+ * @brief Resets to 0 all bits in bitarray
+ * @throw EINVAL If bitarray is NULL
+ * 
+ * @param[in, out] bitarray Bitarray reference
+ * @return If the operation succeeded
+ */
 bool bitarray_clear(bitarray_t *bitarray);
 
-size_t bitarray_count(bitarray_t *bitarray);
+/**
+ * @brief Constructs a bitarray initialized with 0
+ * @throw EINVAL If size is 0
+ * @throw ENOMEM If there is not enough memory
+ * 
+ * @param[in] size Number of bits
+ * @return Bitarray reference or NULL in errors
+ */
+bitarray_t *bitarray_construct(size_t size);
 
-size_t bitarray_size(bitarray_t *bitarray);
-size_t bitarray_buckets(bitarray_t *bitarray);
+/**
+ * @brief Copies the bitarray
+ * @throw EINVAL If bitarray is NULL
+ * @throw ENOMEM If there is not enough memory
+ * 
+ * @param[in] bitarray Bitarray reference
+ * @return Copied bitarray reference or NULL in errors
+ */
+bitarray_t *bitarray_copy(const bitarray_t *bitarray);
+
+/**
+ * @brief Counts the number of bits set to 1 in bitarray
+ * @throw EINVAL If bitarray is NULL
+ * 
+ * @param[in] bitarray Bitarray reference
+ * @return Number of bits set to 1 in bitarray or 0 in errors
+ */
+size_t bitarray_count(const bitarray_t *bitarray);
+
+/**
+ * @brief Sets to 1 all bits in bitarray
+ * @throw EINVAL If bitarray is NULL
+ * 
+ * @param[in, out] bitarray Bitarray reference
+ * @return If the operation succeeded
+ */
+bool bitarray_fill(bitarray_t *bitarray);
+
+/**
+ * @brief Flips the bit at index
+ * @throw EINVAL If bitarray is NULL or index is out of range
+ * 
+ * @param[in, out] bitarray Bitarray reference
+ * @param[in] index Bit index
+ * @return If the operation succeeded
+ */
+bool bitarray_flip(bitarray_t *bitarray, size_t index);
+
+/**
+ * @brief Frees the memory used by bitarray
+ * 
+ * @param[in, out] bitarray Bitarray reference
+ */
+void bitarray_free(bitarray_t *bitarray);
+
+/**
+ * @brief Tests if no bit in bitarray is set to 1
+ * @throw EINVAL If bitarray is NULL
+ * 
+ * @param[in] bitarray Bitarray reference
+ * @return If no bit in bitarray is set to 1 or false in errors
+ */
+bool bitarray_none(const bitarray_t *bitarray);
+
+/**
+ * @brief Performs a not operation
+ * @throw EINVAL If bitarray is NULL
+ * @throw ENOMEM If there is not enough memory
+ * 
+ * @param[in] bitarray Operand reference
+ * @return Resultant bitarray reference or NULL in errors
+ */
+bitarray_t *bitarray_not(const bitarray_t *bitarray);
+
+/**
+ * @brief Performs an or operation
+ * @throw EINVAL If first or second are NULL
+ * @throw ENOMEM If there is not enough memory
+ * 
+ * @param[in] first First operand reference
+ * @param[in] second Second operand reference
+ * @return Resultant bitarray reference or NULL in errors
+ */
+bitarray_t *bitarray_or(const bitarray_t *first, const bitarray_t *second);
+
+/**
+ * @brief Resets to 0 the bit at index
+ * @throw EINVAL If bitarray is NULL or index is out of range
+ * 
+ * @param[in, out] bitarray Bitarray reference
+ * @param[in] index Bit index
+ * @return If the operation succeeded
+ */
+bool bitarray_reset(bitarray_t *bitarray, size_t index);
+
+/**
+ * @brief Sets to 1 the bit at index
+ * @throw EINVAL If bitarray is NULL or index is out of range
+ * 
+ * @param[in, out] bitarray Bitarray reference
+ * @param[in] index Bit index
+ * @return If the operation succeeded
+ */
+bool bitarray_set(bitarray_t *bitarray, size_t index);
+
+/**
+ * @brief Gets the bitarray size
+ * @throw EINVAL If bitarray is NULL
+ * 
+ * @param[in] bitarray Bitarray reference
+ * @return The bitarray size or 0 in errors
+ */
+size_t bitarray_size(const bitarray_t *bitarray);
+
+/**
+ * @brief Tests if the bit at index is set to 1
+ * @throw EINVAL If bitarray is NULL or index is out of range
+ * 
+ * @param[in] bitarray Bitarray reference
+ * @param[in] index Bit index
+ * @return If the bit at index is set to 1 or false in errors
+ */
+bool bitarray_test(const bitarray_t *bitarray, size_t index);
+
+/**
+ * @brief Performs a xor operation
+ * @throw EINVAL If first or second are NULL
+ * @throw ENOMEM If there is not enough memory
+ * 
+ * @param[in] first First operand reference
+ * @param[in] second Second operand reference
+ * @return Resultant bitarray reference or NULL in errors
+ */
+bitarray_t *bitarray_xor(const bitarray_t *first, const bitarray_t *second);
+
 
 #endif // BITARRAY_H
